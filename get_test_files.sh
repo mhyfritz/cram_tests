9de1e5873e1d71eaa3471af2ed5d80a38370605a
mhyfritz <markus.hsi.yang@gmail.com>
2013-02-21 14:29:08 +0100

    added test file sync script

--- get_test_files.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/bam
-wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/ref
---
+++ sync_test_files.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+
+use warnings;
+use strict;
+use File::Basename;
+
+system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/bam";
+system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/ref";
+
+open IN, "<", "bam/.listing";
+my %ftp_files = ();
+while (my $line = <IN>) {
+    my ($fn) = $line =~ /.+?(\S+)\s*$/;
+    $ftp_files{$fn} = 1;
+}
+
+my @local_files = glob("bam/*.bam bam/*.sam.gz");
+foreach my $fn(@local_files) {
+    if (!exists $ftp_files{basename($fn)}) {
+        print "removing obsolete file $fn\n";
+        unlink $fn;
+    }
+}
