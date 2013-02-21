40f3dd4f3aa803cce91451a0ac9285fed482d1d7
mhyfritz <markus.hsi.yang@gmail.com>
2013-02-21 14:39:10 +0100

    cosmetic change

--- sync_test_files.pl
+++ sync_test_files.pl
@@ -8,23 +8,17 @@ system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databa
 system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/ref";
 
 my %ftp_files = ();
-
-open IN, "<", "bam/.listing";
-while (my $line = <IN>) {
-    my ($fn) = $line =~ /.+?(\S+)\s*$/;
-    $ftp_files{$fn} = 1;
+foreach my $listing_file (("bam/.listing", "ref/.listing")) {
+    open IN, "<", $listing_file;
+    while (my $line = <IN>) {
+        my ($fn) = $line =~ /.+?(\S+)\s*$/;
+        $ftp_files{$fn} = 1;
+    }
+    close IN;
 }
-close IN;
-
-open IN, "<", "ref/.listing";
-while (my $line = <IN>) {
-    my ($fn) = $line =~ /.+?(\S+)\s*$/;
-    $ftp_files{$fn} = 1;
-}
-close IN;
 
 my @local_files = glob("bam/*.bam bam/*.sam.gz ref/*.fa ref/*.fai");
-foreach my $fn(@local_files) {
+foreach my $fn (@local_files) {
     if (!exists $ftp_files{basename($fn)}) {
         print "removing obsolete file $fn\n";
         unlink $fn;
