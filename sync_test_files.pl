9c78d01765ce0996ef667bba85c35c99fe11e096
mhyfritz <markus.hsi.yang@gmail.com>
2013-02-21 14:34:56 +0100

    syncs ref files now too

--- sync_test_files.pl
+++ sync_test_files.pl
@@ -7,14 +7,23 @@ use File::Basename;
 system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/bam";
 system "wget --mirror --continue -nH --cut-dirs=5 ftp://ftp.ebi.ac.uk/pub/databases/ena/cram/cramtests/ref";
 
-open IN, "<", "bam/.listing";
 my %ftp_files = ();
+
+open IN, "<", "bam/.listing";
 while (my $line = <IN>) {
     my ($fn) = $line =~ /.+?(\S+)\s*$/;
     $ftp_files{$fn} = 1;
 }
+close IN;
 
-my @local_files = glob("bam/*.bam bam/*.sam.gz");
+open IN, "<", "ref/.listing";
+while (my $line = <IN>) {
+    my ($fn) = $line =~ /.+?(\S+)\s*$/;
+    $ftp_files{$fn} = 1;
+}
+close IN;
+
+my @local_files = glob("bam/*.bam bam/*.sam.gz ref/*.fa ref/*.fai");
 foreach my $fn(@local_files) {
     if (!exists $ftp_files{basename($fn)}) {
         print "removing obsolete file $fn\n";
